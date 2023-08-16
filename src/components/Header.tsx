129a6d146660299139371bf6423180d2fe178ba5
Chris Seaman <christopherseaman1983@gmail.com>
2023-08-16 14:38:06 +0100

    update

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>FootieHub</h1>
+      <nav>
+        <ul>
+          <li>Blogs</li>
+          <li>Newsletters</li>
+          <li>Videos</li>
+          <li>Podcasts</li>
+          <li>Resources</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
