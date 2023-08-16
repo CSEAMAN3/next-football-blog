7abbe860339aa07e42142ebce62c6c6dda501d76
Chris Seaman <christopherseaman1983@gmail.com>
2023-08-16 15:06:59 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${poppins.className} font-normal`}>
         <Header />
         {children}
         <Footer />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 export default function Header() {
   return (
-    <header>
-      <h1>FootieHub</h1>
+    <header className="p-8 flex justify-between border-b-2 border-gray-200">
+      <h1 className="font-bold">FootieHub</h1>
       <nav>
-        <ul>
-          <li>Blogs</li>
-          <li>Newsletters</li>
-          <li>Videos</li>
-          <li>Podcasts</li>
-          <li>Resources</li>
+        <ul className="flex">
+          <li className="ml-8 cursor-pointer">Blogs</li>
+          <li className="ml-8 cursor-pointer">Newsletters</li>
+          <li className="ml-8 cursor-pointer">Videos</li>
+          <li className="ml-8 cursor-pointer">Podcasts</li>
+          <li className="ml-8 cursor-pointer">Resources</li>
         </ul>
       </nav>
     </header>
